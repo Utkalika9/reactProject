37c57ccd17719f64cddc260059fb08cf76019c2c
Utkalika9 Nayak <utkalika.na@gmail.com>
2024-09-28 10:22:51 +0000

    add child component and css style

---
+++ axios-lab/src/components/PersonList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './PersonList.css';
+
+const PersonList = () => {
+    return (
+        <div className="person-list">
+            <h2>
+                Person List
+            </h2>
+
+        </div>
+    )
+}
+export default PersonList;
